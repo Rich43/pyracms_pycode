08489bdd4e76c891e5d7976ec14f8f6901d5219d
Richie Ward <RichieS@GMail.com>
2015-11-29 15:08:18 +0000

    Fix template path

--- pyracms_pycode/views.py
+++ pyracms_pycode/views.py
@@ -1,7 +1,7 @@
 from .lib.pycodelib import PyCodeLib
 from pyramid.view import view_config
 
-@view_config(route_name='show_album', renderer='gallery/pycode.jinja2')
+@view_config(route_name='show_album', renderer='pycode/pycode.jinja2')
 def show_album(context, request):
     g = PyCodeLib()
     a_id = request.matchdict.get('a_id')
